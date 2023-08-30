0da046c4c3ebc0bd6be29cc88749e96dc362a120
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-08-30 18:07:52 +0300

    i hate this :)

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 
-from .views import AllPostView, PostCreateView, PostDeleteView, PostDetailView, PostUpdateView, UsersPostsView, HomeView
+from .views import (AllPostView, HomeView, NotPublishedUsersPostsView, PostCreateView, PostDeleteView, PostDetailView,
+                    PostUpdateView, UserEditView, UserProfileView, UsersPostsView)
 
 
 app_name = 'blog'
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
     path('all-posts/', AllPostView.as_view(), name='all-posts'),
-    path('users-posts/', UsersPostsView.as_view(), name='users-posts'),
+    path('users-posts/<int:pk>/', UsersPostsView.as_view(), name='users-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post'),
     path('post/create/', PostCreateView.as_view(), name='create-post'),
     path('post/update/<int:pk>/', PostUpdateView.as_view(), name='update-post'),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='delete-post'),
+    path('profile/<int:pk>/', UserProfileView.as_view(), name='user-profile'),
+    path('edit-profile/<int:pk>/', UserEditView.as_view(), name='user-edit-profile'),
+    path('not-published-posts/', NotPublishedUsersPostsView.as_view(), name='not-published-posts')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,36 @@
-from blog.forms import PostForm
-from blog.models import Post
+from blog.forms import CommentForm, PostForm
+from blog.models import Comment, Post
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator  # noqa: F401
-from django.shortcuts import render  # noqa: F401
+from django.shortcuts import get_object_or_404, redirect, render  # noqa: F401
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import View, generic
 
-# def all_posts_view(request):
-#         objs = (((Post.objects
-#                 .filter(approved=True, is_published=True))
-#                 .select_related('user'))
-#                 .order_by('-updated_at'))
-#         paginator = Paginator(objs, 10)
-#         page = request.GET.get('page')
-#         page_obj = paginator.get_page(page)
-#         return render(request, 'blog/all_post_list.html', {'page_obj': page_obj, "is_paginated": True})
+
 class HomeView(generic.TemplateView):
     template_name = 'blog/home.html'
 
+
+class UserProfileView(generic.DetailView):
+    model = User
+    template_name = 'blog/profile.html'
+
+
+# TODO: зробити можливість редагування профілю тільки для юзеру який залогінився
+class UserEditView(LoginRequiredMixin, generic.UpdateView):
+    model = User
+    template_name = 'blog/edit_profile.html'
+    fields = ['username', 'email', 'first_name', 'last_name']
+
+    def get_success_url(self):
+        return reverse_lazy('blog:user-profile', kwargs={'pk': self.request.user.id})
+
+
 class AllPostView(generic.ListView):
     model = Post
-    paginate_by = 12
+    paginate_by = 5
     template_name = 'blog/all_post_list.html'
 
     def get_queryset(self):
@@ -33,24 +43,51 @@ class AllPostView(generic.ListView):
 class UsersPostsView(generic.ListView):
     model = Post
     paginate_by = 12
-    template_name = 'blog/all_post_list.html'
+    template_name = 'blog/users_posts.html'
 
     def get_queryset(self):
         return (super(UsersPostsView, self).get_queryset()
-                .filter(is_published=True, owner=self.request.user.id)
+                .filter(is_published=True, approved=True,  owner=self.kwargs.get('pk'))
                 .select_related('owner')
                 .order_by('-updated_at'))
 
 
-class PostDetailView(generic.DetailView):
+class NotPublishedUsersPostsView(LoginRequiredMixin, generic.ListView):
     model = Post
-    template_name = 'blog/post_detail.html'
+    template_name = 'blog/not_published_posts.html'
+    paginate_by = 12
 
     def get_queryset(self):
-        return super(PostDetailView, self).get_queryset().filter().select_related('owner')
+        return (super(NotPublishedUsersPostsView, self).get_queryset()
+                # TODO розібратися з аутентифікацією, треба щоб неопубліковані пости ачили тільки авторизовані
+                #  користувачі і бічили тільки свої пости
+                .filter(is_published=False, owner=self.request.user))
 
 
-class PostCreateView(generic.CreateView):
+class PostDetailView(View):
+    template_name = 'blog/post_detail.html'
+
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        comments = Comment.objects.filter(is_published=True, post=post)
+        form = CommentForm()
+        return render(request, self.template_name, {'post': post, 'comments': comments, 'form': form})
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        comments = Comment.objects.filter(is_published=True, post=post)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            if self.request.user.is_authenticated:
+                comment.owner = request.user
+            comment.save()
+            return redirect('blog:post', pk=pk)
+        return render(request, self.template_name, {'post': post, 'comments': comments, 'form': form})
+
+
+class PostCreateView(LoginRequiredMixin, generic.CreateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
@@ -64,7 +101,7 @@ class PostCreateView(generic.CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(generic.UpdateView):
+class PostUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
@@ -75,18 +112,12 @@ class PostUpdateView(generic.UpdateView):
     def form_valid(self, form):
         if self.request.user.is_authenticated:
             form.instance.owner = self.request.user
+        if form.instance.approved:
+            form.instance.is_published = True
         return super().form_valid(form)
 
 
-class PostDeleteView(generic.DeleteView):
+class PostDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Post
     success_url = reverse_lazy('blog:users-posts')
     template_name = 'blog/post_delete.html'
-
-
-class CreateCommentView(generic.CreateView):
-    ...
-
-
-class DeleteCommentView(generic.DeleteView):
-    ...
