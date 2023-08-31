9fe3e77f1abc58f441f1dd1985001e5ae3319dec
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-08-31 18:04:21 +0300

    updated blog`s views

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 
-from .views import (AllPostView, HomeView, NotPublishedUsersPostsView, PostCreateView, PostDeleteView, PostDetailView,
-                    PostUpdateView, UsersPostsView)
-
+from .views import (AllPostView, HomeView, LoginUserPostsView, NotPublishedLoginUserPostsView, PostCreateView,
+                    PostDeleteView, PostDetailView, PostUpdateView, UserPostsView)
 
 app_name = 'blog'
 urlpatterns = [
-    path('home/', HomeView.as_view(), name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('all-posts/', AllPostView.as_view(), name='all-posts'),
-    path('users-posts/<int:pk>/', UsersPostsView.as_view(), name='users-posts'),
+    path('my-posts/', LoginUserPostsView.as_view(), name='my-posts'),
+    path('users-posts/<int:pk>/', UserPostsView.as_view(), name='users-posts'),
+    path('my-drafts/', NotPublishedLoginUserPostsView.as_view(), name='my-drafts'),
+
+
     path('post/<int:pk>/', PostDetailView.as_view(), name='post'),
     path('post/create/', PostCreateView.as_view(), name='create-post'),
     path('post/update/<int:pk>/', PostUpdateView.as_view(), name='update-post'),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='delete-post'),
-    path('not-published-posts/', NotPublishedUsersPostsView.as_view(), name='not-published-posts')
 ]
